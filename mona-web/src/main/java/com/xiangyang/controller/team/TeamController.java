1d76e2812f3ad0383a1c098ef69cd93cc77ad707
peiji <peiji_chen@outlook.com>
2017-03-24 22:22:18 +0800

    团队管理一期

--- mona-web/src/main/java/com/xiangyang/controller/team/TeamController.java
+++ mona-web/src/main/java/com/xiangyang/controller/team/TeamController.java
@@ -1,8 +1,15 @@
 package com.xiangyang.controller.team;
 
+import com.xiangyang.AO.TeamAO;
+import com.xiangyang.BizResult;
+import com.xiangyang.VO.TeamVO;
+import com.xiangyang.form.team.QueryTeamForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by peiji on 2017/3/20.
@@ -10,10 +17,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/team")
 public class TeamController {
+    @Autowired
+    TeamAO teamAO;
 
+    /**
+     * 获取team信息
+     * @param modelMap
+     */
     @RequestMapping("teamList.htm")
-    public void getTeamList(ModelMap modelMap){
-
-
+    public String getTeamList(ModelMap modelMap){
+        QueryTeamForm queryTeamForm = new QueryTeamForm();
+        queryTeamForm.setPageSize(20);
+        queryTeamForm.setPageNo(1);
+        BizResult<List<TeamVO>> bizResult = new BizResult();
+        bizResult = teamAO.getTeamListInPage(queryTeamForm);
+        modelMap.addAttribute("teamList",bizResult.getResult());
+        return "/team/teamList";
     }
 }
